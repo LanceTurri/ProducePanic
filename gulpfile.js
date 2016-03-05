e6d6321500e9ace2cb2d5d6b8290fcb942b8833d
Lance Turri <lance.turri@gmail.com>
2016-03-05 12:14:44 -0600

    Updated HTML and CSS, changed form box at the bottom, and other minor optimizations.

--- gulpfile.js
+++ gulpfile.js
@@ -12,11 +12,10 @@ gulp.task('sass', function () {
 });
 
 gulp.task('images', function() {
-    return gulp.src('images/*')
+    return gulp.src('images/art/*')
         .pipe(imagemin({
             progressive: true,
             svgoPlugins: [{removeViewBox: false}],
-            use: [pngquant()]
         }))
         .pipe(gulp.dest('images'));
 });
